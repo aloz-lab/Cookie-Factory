0973d148c27fec27214080f1804bb6b9466133af
aloz-lab <alozier45@gmail.com>
2020-04-27 22:54:49 +0200

    first commit

---
+++ cart.php
@@ -0,0 +1,41 @@
+<?php require 'inc/head.php'; ?>
+<?php require 'inc/data/products.php'; ?>
+<section class="cookies container-fluid">
+    <div class="row">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Référence</th>
+                    <th>Produit</th>
+                    <th>Prix unitaire</th>
+                    <th>Quantité</th>
+                    <th>Prix total</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php $totalPrice = 0; ?>
+            <?php foreach ($_SESSION['cart'] as $productId => $quantities) { ?>
+                <tr>
+                    <td><?= $productId;?></td>
+                    <td><?= $catalog[$productId]['name'];?></td>
+                    <td><?= $catalog[$productId]['price'];?> €</td>
+                    <td><?= $quantities;?></td>
+                    <td><?= $rowTotalPrice = $quantities * $catalog[$productId]['price'];?> €</td>
+                </tr>
+                <?php $totalPrice += $rowTotalPrice; ?>
+            <?php } ?>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colspan="4">
+                        Total :
+                    </td>
+                    <td>
+                        <?= $totalPrice; ?> €
+                    </td>
+                </tr>
+            </tfoot>
+        </table>
+    </div>
+</section>
+<?php require 'inc/foot.php'; ?>
